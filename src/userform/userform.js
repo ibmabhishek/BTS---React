7ebc2060435084577bb7b3a45647ee93b922ccb2
ibmabhishek <erren98@gmail.com>
2021-04-26 14:50:11 +0530

    Added button

--- src/userform/userform.js
+++ src/userform/userform.js
@@ -3,19 +3,32 @@ import { useState } from 'react';
 function Userform() { //Component, Controller
     //model
     const [userform, setUserform] = useState({
-        name: 'Ram'
+        name: 'Ram',
+        age: 23,
     })
+    // const save(){
+
+    // }
     return (//View
         <div>
-            <input value={userform.name} onChange={handleEvent}></input>
+            <input value={userform.name} onChange={handleEvent} ></input>
+            <input value={userform.age} onChange={handleEvent}></input>
+
+            <button onClick={save} >Save</button>
         </div>
     )
+
+    function save() {
+        console.log(userform.name);
+        console.log(userform.age);
+    }
 
     function handleEvent(event) {
         console.log(event);
         setUserform({
 
-            name: event.target.value
+            name: event.target.value,
+            age: event.target.value
         })
         // setState
     }
