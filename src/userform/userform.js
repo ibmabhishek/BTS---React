3db013e5dfc07a28025d429b26c3cfebcb297717
ibmabhishek <erren98@gmail.com>
2021-04-26 16:59:27 +0530

    Post method completed

--- src/userform/userform.js
+++ src/userform/userform.js
@@ -1,35 +1,45 @@
+import axios from 'axios';
 import { useState } from 'react';
 
 function Userform() { //Component, Controller
     //model
     const [userform, setUserform] = useState({
         name: 'Ram',
-        age: 23,
+        mobileNumber: "1"
+
     })
     // const save(){
 
     // }
+    const save = function (event) {
+        console.log(userform.name);
+        axios
+            .post('http://localhost:8080/user', userform)
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        //console.log(userform.age);
+    }
     return (//View
         <div>
-            <input value={userform.name} onChange={handleEvent} ></input>
-            <input value={userform.age} onChange={handleEvent}></input>
+            <input value={userform.name} name='name' onChange={updateState} ></input>
+            <input value={userform.mobileNumber} name='mobileNumber' placeholder='Enter the mobile  number' onChange={updateState}></input>
 
             <button onClick={save} >Save</button>
         </div>
     )
 
-    function save() {
-        console.log(userform.name);
-        console.log(userform.age);
-    }
 
-    function handleEvent(event) {
-        console.log(event);
-        setUserform({
 
-            name: event.target.value,
-            age: event.target.value
-        })
+    function updateState(event) {
+        //console.log(event);
+        const current = { ...userform, [event.target.name]: event.target.value };
+        console.log(current);
+        setUserform(current);
+
         // setState
     }
 
